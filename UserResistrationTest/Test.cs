9244dcb3e98ad55419f56a8ae23a4c47d2e8123d
poojasri <poojasrijampani@gmail.com>
2020-10-30 15:52:07 +0530

    Added: Valid first name for user registration

--- UserResistrationTest/Test.cs
+++ UserResistrationTest/Test.cs
@@ -6,16 +6,26 @@ namespace UserResistrationTest
     public class Tests
     {
         UserRegistrationMain userRegistration = new UserRegistrationMain();
+        
         [SetUp]
         public void Setup()
         {
         }
 
+        //Test Method to validate first name
         [Test]
-        public void firstName()
+        public void GivenProperFirstName_ShouldReturnTrue()
         {
             bool result = userRegistration.firstNameValidater("Pooja");
             Assert.True(result);
+        }
+
+        //Test Method to Invalidate first name
+        [Test]
+        public void GivenInvalidFirstName_ShouldReturnFalse()
+        {
+            bool result = userRegistration.firstNameValidater("po");
+            Assert.False(result);
         }
     }
 }
