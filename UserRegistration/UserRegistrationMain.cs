a7318cab565275ccf91b11b047fe2eb79e45642d
poojasri <poojasrijampani@gmail.com>
2020-10-30 16:23:46 +0530

    Added: Valid last name for user registration

--- UserRegistration/UserRegistrationMain.cs
+++ UserRegistration/UserRegistrationMain.cs
@@ -7,10 +7,19 @@ namespace UserRegistration
 {
     public class UserRegistrationMain
     {
+        //Name regular expression pattern
         public string NAMEPATTERN = "^[A-Z]{1}[a-z]{2,}$";
+
+
+
         public bool firstNameValidater(string firstName)
         {
-            return Regex.Match(firstName, this.NAMEPATTERN).Success;
+            return Regex.Match(firstName, NAMEPATTERN).Success;
+        }
+
+        public bool lastNameValidater(string lastNmae)
+        {
+            return Regex.Match(lastNmae, NAMEPATTERN).Success;
         }
     }
 }
