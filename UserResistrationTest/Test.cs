4b0bdd21727922cd10456987ff5c33eed2d50f56
poojasri <poojasrijampani@gmail.com>
2020-10-30 19:13:27 +0530

    Refactor: Code to check valid password for registration

--- UserResistrationTest/Test.cs
+++ UserResistrationTest/Test.cs
@@ -6,7 +6,7 @@ namespace UserResistrationTest
     public class Tests
     {
         UserRegistrationMain userRegistration = new UserRegistrationMain();
-        
+
         [SetUp]
         public void Setup()
         {
@@ -44,5 +44,101 @@ namespace UserResistrationTest
             Assert.False(result);
         }
 
+        //Test Method to validate mailid
+        [Test]
+        public void GivenProperMailId_ShouldReturnTrue()
+        {
+            bool result = userRegistration.mailIdValidater("pooja@gmail.com");
+            Assert.True(result);
+        }
+
+        //Test Method to Invalidate mailid
+        [Test]
+        public void GivenInvalidMailId_ShouldReturnFalse()
+        {
+            bool result = userRegistration.mailIdValidater("@pooja.123");
+            Assert.False(result);
+        }
+
+        //Test Method to validate mobile number
+        [Test]
+        public void GivenProperPhonenumber_ShouldReturnTrue()
+        {
+            bool result = userRegistration.PhoneNumberValidater("91 9963316462");
+            Assert.True(result);
+        }
+
+        //Test Method to Invalidate mobile number
+        [Test]
+        public void GivenInvalidPhoneNumber_ShouldReturnFalse()
+        {
+            bool result = userRegistration.PhoneNumberValidater("9132 3464");
+            Assert.False(result);
+        }
+
+        //Test Method to validate password
+        [Test]
+        public void GivenMin8Character_ShouldReturnTrue()
+        {
+            bool result = userRegistration.minimumEightValidater("Pooja@123");
+            Assert.True(result);
+        }
+
+        //Test Method to Invalidate password
+        [Test]
+        public void GivenLessthan8Character_ShouldReturnFalse()
+        {
+            bool result = userRegistration.PhoneNumberValidater("pooja");
+            Assert.False(result);
+        }
+
+        //Test Method to validate password
+        [Test]
+        public void GivenOneUpperCase_ShouldReturnTrue()
+        {
+            bool result = userRegistration.minimumUpperCase("Pooja@123");
+            Assert.True(result);
+        }
+
+        //Test Method to Invalidate password
+        [Test]
+        public void GivenInvalidUpperCase_ShouldReturnFalse()
+        {
+            bool result = userRegistration.PhoneNumberValidater("pooja13344");
+            Assert.False(result);
+        }
+
+        //Test Method to validate password
+        [Test]
+        public void GivenNumeric_ShouldReturnTrue()
+        {
+            bool result = userRegistration.numericValidater("Pooja@123");
+            Assert.True(result);
+        }
+
+        //Test Method to Invalidate password
+        [Test]
+        public void GivenIncorrectInput_ShouldReturnFalse()
+        {
+            bool result = userRegistration.numericValidater("pooja");
+            Assert.False(result);
+        }
+
+
+        //Test Method to validate password
+        [Test]
+        public void GivenSpecialcharacter_ShouldReturnTrue()
+        {
+            bool result = userRegistration.specialCharValidater("Pooja@123");
+            Assert.True(result);
+        }
+
+        //Test Method to Invalidate password
+        [Test]
+        public void GivenIncorrectPassword_ShouldReturnFalse()
+        {
+            bool result = userRegistration.specialCharValidater("pooja");
+            Assert.False(result);
+        }
     }
 }
